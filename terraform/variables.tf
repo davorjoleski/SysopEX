8603127cf47f7310d5e7aac10473b26e678cb323
Davor <davor.j@live.com>
2025-09-13 20:21:06 +0200

    CI/cd Terrafomr provisioning

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,8 +1,22 @@
-variable "client_id" {}
-variable "client_secret" {}
-variable "subscription_id" {}
-variable "tenant_id" {}
+variable "client_id" {
+  type        = string
+  description = "Azure Client ID"
+}
 
+variable "client_secret" {
+  type        = string
+  description = "Azure Client Secret"
+}
+
+variable "tenant_id" {
+  type        = string
+  description = "Azure Tenant ID"
+}
+
+variable "subscription_id" {
+  type        = string
+  description = "Azure Subscription ID"
+}
 
 variable "location" {
   description = "Azure region"
--- terraform/versions.tf
+++ terraform/versions.tf
@@ -16,8 +16,9 @@ terraform {
 provider "azurerm" {
   features {}
 
-        subscription_id = var.subscription_id
-        client_id       = var.client_id
-        client_secret   = var.client_secret
-        tenant_id       = var.tenant_id
+
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
 }
