90171a12fd978b48d3e56bfa13d8d9635f88a738
Davor <davor.j@live.com>
2025-09-13 16:22:33 +0200

    CI/cd Terrafomr provisioning

--- terraform/acr.tf
+++ terraform/acr.tf
@@ -7,9 +7,3 @@ resource "azurerm_container_registry" "acr" {
 
 }
 
-# Role assignment za acrpull
-resource "azurerm_role_assignment" "acr_pull" {
-  scope                = azurerm_container_registry.acr.id
-  role_definition_name = "AcrPull"
-  principal_id         = azurerm_kubernetes_cluster.aks.identity[0].principal_id
-}
