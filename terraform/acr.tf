c50615a490bd80641b0fecfddd93639115840854
Davor <davor.j@live.com>
2025-09-16 14:00:18 +0200

    CI/cd Terrafomr provisioning

--- terraform/acr.tf
+++ terraform/acr.tf
@@ -6,6 +6,12 @@ resource "azurerm_container_registry" "acr" {
   admin_enabled       = true
 
 }
+provider "kubernetes" {
+  host                   = azurerm_kubernetes_cluster.aks.kube_config[0].host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate)
+}
 
 resource "kubernetes_secret" "acr_secret" {
   metadata {
