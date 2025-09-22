a077dc8f999149b9258fb7acc135782db5c6e4dc
Davor <davor.j@live.com>
2025-09-22 22:18:25 +0200

    Changes and add new resources

--- terraform/role_assignments.tf
+++ terraform/role_assignments.tf
@@ -5,7 +5,7 @@ resource "azurerm_role_assignment" "acr_pull" {
   scope                = azurerm_container_registry.acr.id
   role_definition_name = "AcrPull"
   # kubelet identity principal id (object_id) се враќа од ресурсот
-  principal_id = azurerm_kubernetes_cluster.aks.identity[0].principal_id
+  principal_id = "59b96b2c-d336-4a86-a7e1-0b1b116cee32"
   skip_service_principal_aad_check = true
   depends_on           = [azurerm_kubernetes_cluster.aks]
 
